2e92d9ae456fe184e1b225f91c9eeb35b4359a00
Rob Holden <robholdenflag@gmail.com>
2017-02-26 13:25:46 +0000

    Created picture model

--- Gemfile
+++ Gemfile
@@ -5,7 +5,10 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-
+# Create views in haml
+gem 'haml'
+# Replaces and erb extensions with hml
+gem 'haml-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.1'
 # Use postgresql as the database for Active Record
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -4,8 +4,19 @@ feature 'pictures' do
   context 'no pictures have been added' do
     scenario 'should display a prompt upload a picture' do
       visit '/pictures'
-      expect(page).to have_content 'No Pictures Uploaded'
+      expect(page).to have_content 'No Pictures Uploaded Yet'
       expect(page).to have_link 'Upload A Picture'
     end
+  end
+
+  context 'restaurants have been added' do
+  before do
+    Restaurant.create(name: 'KFC')
+  end
+
+  scenario 'display restaurants' do
+    visit '/restaurants'
+    expect(page).to have_content('KFC')
+    expect(page).not_to have_content('No restaurants yet')
   end
 end
